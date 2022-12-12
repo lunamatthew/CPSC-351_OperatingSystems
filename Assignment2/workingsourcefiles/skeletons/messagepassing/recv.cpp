90a355821f1ba656198b5c78bc32861bbded4a6b
Matthew Luna <57422802+lunamatthew@users.noreply.github.com>
2022-12-11 17:29:48 -0800

    Add files via upload

--- Assignment2/workingsourcefiles/skeletons/messagepassing/recv.cpp
+++ Assignment2/workingsourcefiles/skeletons/messagepassing/recv.cpp
@@ -10,99 +10,47 @@
 #include <unistd.h>
 #include <signal.h>
 
-// The name of the shared memory segment
 #define MSQ_NAME "/cpsc351queue"
-
-// The name of the file where to save the received file
 #define RECV_FILE_NAME "file__recv"
 
 #define MQ_MSGSIZE 4096
 
-
-/**
- * Receive the file from the message queue 
- */
 void recvFile()
 {
-	// TODO: Define a data structure
-	// of type mq_attr to specify a
-	// queue that can hold up to 10
-	// messages with the maximum message
-	// size being 4096 bytes
 	mqd_t myQueue;
 	unsigned int prio = 0;	
 	mq_attr attr;
-	attr.mq_maxmsg = 10;	// possibly not necessary?
-	attr.mq_msgsize = MQ_MSGSIZE; // possibly not necessary?
+	attr.mq_maxmsg = 10;
+	attr.mq_msgsize = MQ_MSGSIZE;
 	attr.mq_curmsgs = 0;
 
-	// The buffer used to store the message copied
-	// copied from the shared memory
 	char buff[MQ_MSGSIZE];
 
-	//unsigned int priority = 1;
-
 	myQueue = mq_open(MSQ_NAME, O_CREAT | O_RDWR, 0600, &attr);
-	// mq_setattr() ? use here ?
 	if (myQueue < 0) {
 		perror("mq_open");
 		exit(-1);
 	}
 
-	// The total number of bytes written
 	int totalBytesWritten = 0;
-
-	// The number of bytes written 
 	int bytesWritten = 0;
-	
-	// The return value
 	int msqBytesRecv = -1;
-	
-	// Open the file for writing
 	int fd = open(RECV_FILE_NAME, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	
-	// Make sure the file was opened
 	if(fd < 0)
 	{
 		perror("open");
 		exit(1);
 	}
 
-
-	
-	// TODO: Create the message queue
-	// whose name is defined by the macro
-	// MSQ_NAME macro above. 
-	// The queue should be configured with
-	// parameters defined by the data 
-	// message queue data structure defined above.
-	// The queue should have permissions of 0600.
 	int tmpFileOps = 0;
-	int i = 0;
 	while(msqBytesRecv != 0)
 	{
-		// TODO: Receive the message.
-		// Write the data from the message
-		// into the file (represented by the 
-		// descriptor fd). Please note:
-		// the mq_receive() function
-		// you will use will return
-		// the size of the message
-		// received (in bytes).
-		// If the size of the message
-		// is 0, it is the sender telling
-		// us that it has no more data to send.
-		// Please see the sender's code for the
-		// corresponding logic
-		++i;
-		msqBytesRecv = mq_receive(myQueue, buff, 8192, &prio);
-		printf("Bytes received: %d Iterations: %d\n Priority: %d ", (int)msqBytesRecv, i, prio);
+		msqBytesRecv = mq_receive(myQueue, buff, 8192, NULL);
 		if (msqBytesRecv < 0) {
 			perror("mq_receive");
 			exit(-1);
 		}
 		if (msqBytesRecv > 0) {
-			//tmpFileOps = write(fd, buff, msqBytesRecv);
 			tmpFileOps = write(fd, buff, msqBytesRecv);
 		}
 	}
@@ -112,7 +60,6 @@ void recvFile()
 		exit(1);
 	}
 
-	// TODO: Deallocate the message queue
 	if (mq_unlink(MSQ_NAME) < 0) {
 		perror("mq_unlink");
 		exit(1);
--- Assignment2/workingsourcefiles/skeletons/messagepassing/sender.cpp
+++ Assignment2/workingsourcefiles/skeletons/messagepassing/sender.cpp
@@ -27,83 +27,48 @@ int getFileSize(const char* fileName) {
 int main(int argc, char** argv)
 {	
 
-	// The file size
 	int fileSize = -1;
-
-	// The buffer used to store the message copied
-	// copied from the shared memory
 	char buff[8192];
-
-	// The variable to hold the message queue ID
 	int msqid = -1;
-
-	// The total number of bytes written
 	int totalBytesRead = 0;
-
-	// The number of bytes 
 	int bytesRead = 0;
-
-	// Whether we are done reading	
 	bool finishedReading = false;
-	
-	// TODO: Define a data structure
-	// of type mq_attr to specify a
-	// queue that can hold up to 10
-	// messages with the maximum message
-	// size being 4096 bytes
 	mqd_t myQueue, retVal;
 	mq_attr attr;
 	attr.mq_maxmsg = 10;
 	attr.mq_msgsize = 4096;
 	unsigned int priority = 1;
 
-	myQueue = mq_open(MSQ_NAME, O_RDWR, 0600, &attr);
-	if (myQueue < 0) {
-		perror("Error with mq_open!");
-		exit(1);
-	}
+	//myQueue = mq_open(MSQ_NAME, O_RDWR, 0600, &attr);
+	//if (myQueue < 0) {
+	//	perror("Error with mq_open!");
+	//	exit(1);
+	//}
 	
-	// Sanity checks -- make sure the user has provided a file
 	if(argc < 2)
 	{
 		fprintf(stderr, "USAGE: %s <FILE NAME>\n", argv[0]);
 		exit(1);
 	}
 
-	// Open the file for reading
 	int fd = open(argv[1], O_RDONLY);
 	
-	// Make sure the file was opened
 	if(fd < 0)
 	{
 		perror("open");
 		exit(1);
 	}
 	
+	myQueue = mq_open(MSQ_NAME, O_RDWR, 0600, &attr);
+	if (myQueue < 0) {
+		perror("Error with mq_open!");
+		exit(1);
+	}
 	
-	// TODO: Gain access to the message queue
-	// whose name is defined by the macro
-	// MSQ_NAME macro above. We assume that
-	// the receiver has allocated the message queue.
-
-	
-	//TODO: Loop below attempts to read the 
-	// file 4096 bytes at a time.
-	// Modify the loop as necessary to send
-	// each chunk of data read as message
-	// through the message queue. You can use
-	// 1 for the priority of the message.
 	fileSize = getFileSize(argv[1]);
-	// Keep writing until all data has been written
-	//fprintf(stderr, "\nqueue msg size: %d\n", (int)attr.mq_msgsize);
-	//fprintf(stderr, "size: %d\n", fileSize);
 	while((totalBytesRead < fileSize) && !finishedReading)
-	//while(!finishedReading)
 	{
-		// Read from file to the shared memory
 		bytesRead = read(fd, buff, 4096);
-		//fprintf(stderr, "bytesRead: %d\n", bytesRead);
-		// Something went wrong
 		if(bytesRead < 0)
 		{
 			perror("read");
@@ -118,21 +83,13 @@ int main(int argc, char** argv)
 			}
 		}
 
-		// We are at the end of file
 		else if (bytesRead == 0)
 		{
-			fprintf(stderr, "TEST!!!!%d\n\n", bytesRead);
-			// We are at the end of file
 			finishedReading = true;		
 		}
 		
-		//fprintf(stderr, "bool: %d\n", (int)finishedReading);
 		totalBytesRead += bytesRead;
 	}
-	
-	// TODO: Send a message with size of 0
-	// to the receiver to tell it that the
-	// transmission is done
 
 	mq_getattr(myQueue, &attr);
 	while (attr.mq_curmsgs != 0) {
@@ -146,7 +103,6 @@ int main(int argc, char** argv)
 	fprintf(stderr, "Sent a total of %d bytes\n", totalBytesRead);
 	
 	fprintf(stderr, "bool: %d\n", (int)finishedReading);
-	// TODO: Close the file		
 	if (close(fd)) {
 		perror("close");
 		exit(1);
